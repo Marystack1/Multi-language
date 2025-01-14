92a6ace28f7612a3e301d3d69c40b007e806c020
Praise-png <praise.dike@binghamuni.edu.ng>
2025-01-14 15:10:28 +0100

    Update Multi-language.py

--- Multi-language.py
+++ Multi-language.py
@@ -136,9 +136,6 @@ Translations = {
              "Japanese":"Ie", }
 }
 
-Label(window, text="Enter a word:").pack()
-input_entry = Entry(window)
-input_entry.pack()
 
 def show_translations():
     """"
